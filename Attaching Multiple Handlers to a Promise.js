e1cdd72846a81ad4a8ef92dc4330ad9f64ad797e
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-26 12:29:34 +0500

    Update Attaching Multiple Handlers to a Promise.js

--- Attaching Multiple Handlers to a Promise.js
+++ Attaching Multiple Handlers to a Promise.js
@@ -1,1 +1,22 @@
+/* We can attach multiple handlers to one promise. They don't pass the result to each other,instead they process it independently */
+
+let p1 = new Promise((resolve, reject) => {
+        // alert("Hey I am not resolved")
+        setTimeout(() => {
+                resolve(1);
+        }, 2000)
+})
+
+p1.then(() => {
+        console.log("Hurray")
+        return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                        resolve(4)
+                }, 6000)
+        })
+}).then((value) => { console.log(value) })
+
+p1.then(() => {
+        console.log("Congratulations this promise is now resolved")
+})
 
