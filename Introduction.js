2fc2b53b322387139f23d136f782c9d2cab74d2f
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-26 09:51:55 +0500

    Update Introduction.js

--- Introduction.js
+++ Introduction.js
@@ -1,3 +1,6 @@
+/* The Solution to the Callback hell is Promises. A Promise is a "Promise of code execution" 
+The code either execute or fails in both the cases the Subscriber will be notified */
+
 
 let promise= new Promise (function(resolve,reject){
   alert("Hello")
@@ -12,3 +15,16 @@ setTimeout(function() {
 
 console.log("My name is" + "Hello Three")
 console.log(promise)
+
+/* resolve & reject are two callbacks provided by JavaScript itself. They are called like this:
+
+resolve(value) : If the Job is Finished Successfully 
+reject(error) : If the job fails 
+
+The Promise object returned by the new promise constructor has these properties
+
+1. state : Initially Pending, then changes to either "fulfilled" when resolve is called or "rejected" when reject is called
+
+2. result: Initially undefined, then changes to value if resolved or error when rejected.
+
+*/
