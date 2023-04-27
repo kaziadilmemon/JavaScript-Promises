0b7e9a1924704094c5c604ee1b2fcd4b17a4ccc9
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-27 23:31:08 +0500

    Update Finally Clause.js

--- Finally Clause.js
+++ Finally Clause.js
@@ -1,11 +1,1 @@
-mber.parseInt(age) 
-        if(age>150){
-        throw new ReferenceError("This is probably not true")
-        }
-} catch (error) {
-        console.log(error.name)
-        console.log(error.message)
-        console.log(error.stack)
-}
 
-console.log("The script is still running")
