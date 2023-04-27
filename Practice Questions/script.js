a2b8a8e16867d5c7f2ed146f298899abcad4ba72
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-27 23:54:28 +0500

    Update script.js

--- Practice Questions/script.js
+++ Practice Questions/script.js
@@ -11,3 +11,16 @@ const loadScript = async (src) => {
 // Problem number 1 : Write a program to find load a JavaScript file in a browser using promises. use.then() to display an alert when the load is Complete.
 
 let a = loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js")
+ a.then((value) => {
+       console.log(value)
+ })
+
+// Problem number 2 : Write the same program from previous Question and use asunc/await Syntax.
+
+ const main2 = async () => {
+         console.log(new Date().getMilliseconds())
+         let a = await loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js")
+         console.log(a)
+         console.log(new Date().getMilliseconds())
+ }
+ main2()
