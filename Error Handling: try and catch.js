319f1f279a6a421c0ee6d1310aad02a59462ec3d
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-27 22:29:28 +0500

    Create Error Handling: try and catch.js

---
+++ Error Handling: try and catch.js
@@ -0,0 +1,25 @@
+/* We all make mistakes. Also sometimes our Script can have errors. usually a program halts when an error occurs
+It works like this:
+1. First the code in try is executed.
+2. If there is no error,catch is ignored else catch is executed.*/
+
+setTimeout(()=>{
+console.log("Hacking wifi.... Please wait..." ) 
+}, 1000)
+
+try{
+        setTimeout(()=>{ 
+                console.log(rahul)  
+        }, 100)
+}
+catch(err){
+        console.log("Error")
+}
+
+
+setTimeout(()=>{ 
+        console.log("Fetching username and password.... Please wait..." )
+}, 2000) 
+
+setTimeout(()=>{ 
+console.log("Hacking Rahul's facebook id
