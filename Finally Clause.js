717a1385eaf372fb8ca97a220b780ad1b752d1e2
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-27 23:31:25 +0500

    Update Finally Clause.js

--- Finally Clause.js
+++ Finally Clause.js
@@ -1,1 +1,15 @@
+/* For all the built in erros,the error object has two main properties(name,message,stack).We can throw our own error by using the throw syntax.*/
 
+try {
+        let age = prompt("Enter your age")
+        age = Number.parseInt(age) 
+        if(age>150){
+        throw new ReferenceError("This is probably not true")
+        }
+} catch (error) {
+        console.log(error.name)
+        console.log(error.message)
+        console.log(error.stack)
+}
+
+console.log("The script is still running")
